3a890f3c2869e603aea815b96d612e57a9ea0709
AlissonRM19 <alisson.r06@estudiantec.cr>
2021-08-27 19:13:16 -0600

    Tarea 1 ventanaChatServidor creada

--- src/Paquete1/Main.java
+++ src/Paquete1/Main.java
@@ -13,13 +13,18 @@ public class Main {
      * Tarea extraclase 1
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater (new Runnable() {
             @Override
             public void run() {
                 JFrame frame = new VentanaChatCliente();
                 frame.setSize(500, 300);
                 frame.setVisible(true);
+
+                JFrame frame2 = new VentanaChatServidor();
+                frame2.setSize(500, 500);
+                frame2.setVisible(true);
             }
         });
+
     }
 }
